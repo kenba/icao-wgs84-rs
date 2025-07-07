2e60f7beeb00196b229b0db204b45674b825f965
Ken Barker <ken.barker@via-technology.aero>
2025-07-07 09:28:53 +0100

    Replace itertools with Dataframe for_each.

--- tests/rtca_do_283b_examples_test.rs
+++ tests/rtca_do_283b_examples_test.rs
@@ -22,79 +22,99 @@ extern crate icao_wgs84;
 
 use angle_sc::{Angle, Degrees, Radians};
 use icao_wgs84::{geodesic, Metres, WGS84_ELLIPSOID};
-use itertools::multizip;
 use polars::prelude::*;
 use unit_sphere::{great_circle, LatLong};
 
 const FILENAME: &str = "data/rtca_do_283b_geodesic_examples.csv";
 
+/// Calculate the geodesic values for the given start and end point latitudes and longitudes.
+///
+/// returns start_azimuth, end_azimuth, distance, arc_distance, iterations
+fn calculate_geodesic_inverse_values(
+    lat1: f64,
+    lon1: f64,
+    lat2: f64,
+    lon2: f64,
+) -> (Angle, Angle, Metres, Radians, u32) {
+    let lat1 = Degrees(lat1);
+    let a = LatLong::new(lat1, Degrees(lon1));
+    let b = LatLong::new(Degrees(lat2), Degrees(lon2));
+    let result = geodesic::calculate_azimuths_arc_length(
+        &a,
+        &b,
+        Radians(great_circle::MIN_VALUE),
+        &WGS84_ELLIPSOID,
+    );
+
+    let beta1 = WGS84_ELLIPSOID.calculate_parametric_latitude(Angle::from(lat1));
+    let result_m = geodesic::convert_radians_to_metres(beta1, result.0, result.1, &WGS84_ELLIPSOID);
+
+    (result.0, result.2, result_m, result.1, result.3)
+}
+
 #[test]
 #[ignore]
 fn test_rtca_do_283b_examples() -> Result<(), Box<dyn std::error::Error>> {
-    // Read file and run tests
-    let df = CsvReadOptions::default()
-        .with_has_header(true)
-        .try_into_reader_with_file_path(Some(FILENAME.into()))?
-        .finish()?;
+    let lf = LazyCsvReader::new(FILENAME).finish()?.collect()?;
+    // println!("{lf}");
 
-    // println!("{:?}", df.head(None));
+    let length = lf.height();
+    let mut index_v = Vec::with_capacity(length);
+    let mut end_azi_v = Vec::with_capacity(length);
+    let mut delta_azimuth_v = Vec::with_capacity(length);
+    let mut delta_length_v = Vec::with_capacity(length);
+    let mut delta_error_v = Vec::with_capacity(length);
 
-    // Use multizip to access DataFrame by rows
-    let objects = df.take_columns();
-    let dep_lats = objects[0].f64()?.iter();
-    let dep_lons = objects[1].f64()?.iter();
-    let arr_lats = objects[2].f64()?.iter();
-    let arr_lons = objects[3].f64()?.iter();
-    let dep_azis = objects[4].f64()?.iter();
-    let ranges = objects[6].f64()?.iter();
-    let range_errors = objects[7].f64()?.iter();
-    let combined = multizip((
-        dep_lats,
-        dep_lons,
-        arr_lats,
-        arr_lons,
-        dep_azis,
-        ranges,
-        range_errors,
-    ));
+    let mut index = 0;
+    lf.column("Departure_Latitude")?
+        .f64()?
+        .into_no_null_iter()
+        .zip(lf.column("Departure_Longitude")?.f64()?.into_no_null_iter())
+        .zip(lf.column("Arrival_Latitude")?.f64()?.into_no_null_iter())
+        .zip(lf.column("Arrival_Longitude")?.f64()?.into_no_null_iter())
+        .zip(lf.column("Departure_Bearing")?.f64()?.into_no_null_iter())
+        // .zip(lf.column("Arrival_Bearing")?.f64()?.into_no_null_iter())
+        .zip(lf.column("Ranges")?.f64()?.into_no_null_iter())
+        .zip(lf.column("Range_Error")?.f64()?.into_no_null_iter())
+        .for_each(
+            |((((((lat1, lon1), lat2), lon2), azi1), d_metres), range_error)| {
+                let (azi, end_azi, distance_m, _arc_length, _iterations) =
+                    calculate_geodesic_inverse_values(lat1, lon1, lat2, lon2);
 
-    println!("line_number,iterations,delta_azimuth,delta_length,delta_error");
-    for (index, (lat1, lon1, lat2, lon2, azi1, d_metres, range_error)) in combined.enumerate() {
-        let lat1 = Degrees(lat1.unwrap());
-        let lon1 = Degrees(lon1.unwrap());
-        let lat2 = Degrees(lat2.unwrap());
-        let lon2 = Degrees(lon2.unwrap());
-        let azi1 = Degrees(azi1.unwrap());
-        // let _azi2 = Degrees(azi2.unwrap());
-        let d_metres = Metres(d_metres.unwrap());
-        let range_error = range_error.unwrap();
+                // Convert azimuths to degrees
+                let azi = Degrees::from(azi).0;
+                let end_azi = Degrees::from(end_azi).0;
 
-        let a = LatLong::new(lat1, lon1);
-        let b = LatLong::new(lat2, lon2);
-        let result = geodesic::calculate_azimuths_arc_length(
-            &a,
-            &b,
-            Radians(great_circle::MIN_VALUE),
-            &WGS84_ELLIPSOID,
+                let delta_azimuth = libm::fabs(azi1 - azi);
+                let delta_length_m = libm::fabs(d_metres - distance_m.0);
+                let delta_error_m = range_error - delta_length_m;
+
+                // push values into vectors
+                index_v.push(index + 1);
+                end_azi_v.push(end_azi);
+                delta_azimuth_v.push(delta_azimuth);
+                delta_length_v.push(delta_length_m);
+                delta_error_v.push(delta_error_m);
+
+                index += 1;
+            },
         );
 
-        let delta_azimuth = libm::fabs(azi1.0 - Degrees::from(result.0).0);
+    // Create a DataFrame for the output values
+    let line_no_column = Column::new("line_no".into(), index_v);
+    let end_azi_column = Column::new("end_azi".into(), end_azi_v);
+    let delta_azi_column = Column::new("delta_azi".into(), delta_azimuth_v);
+    let delta_length_column = Column::new("delta_length".into(), delta_length_v);
+    let delta_error_column = Column::new("delta_error".into(), delta_error_v);
+    let df = DataFrame::new(vec![
+        line_no_column,
+        end_azi_column,
+        delta_azi_column,
+        delta_length_column,
+        delta_error_column,
+    ])?;
 
-        let beta1 = WGS84_ELLIPSOID.calculate_parametric_latitude(Angle::from(lat1));
-        let result_m =
-            geodesic::convert_radians_to_metres(beta1, result.0, result.1, &WGS84_ELLIPSOID);
-
-        let delta_length_m = libm::fabs(d_metres.0 - result_m.0);
-        let delta_error_m = range_error - delta_length_m;
-        println!(
-            "{},{},{},{},{}",
-            index + 1,
-            result.3,
-            delta_azimuth,
-            delta_length_m,
-            delta_error_m
-        );
-    }
+    println!("{df}");
 
     Ok(())
 }
